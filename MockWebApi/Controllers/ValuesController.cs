6c8b3a3920df80cf53238f40bfc2634b6ea08baf
Timothy Hayes <tjhayes011@gmail.com>
2018-06-05 11:07:06 -0400

    modify values controller

--- MockWebApi/Controllers/ValuesController.cs
+++ MockWebApi/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace MockWebApi.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value #"+id;
         }
 
         // POST api/values
