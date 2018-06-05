72ecad38854a25f92812281f0b14064a0e7f5d59
Timothy Hayes <tjhayes011@gmail.com>
2018-06-05 15:21:58 -0400

    add fake Db class for testing

---
+++ FakeDb/Db.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FakeDb
+{
+    public class Db : IEnumerable<object[]>
+    {
+        private List<object[]> _data;
+
+        public Db()
+        {
+            _data = new List<object[]>();
+            for (int i = 0; i < 10; i++)
+            {
+                _data.Add(new object[] { i, i.ToString() });
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator<object[]> IEnumerable<object[]>.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+    }
+}
