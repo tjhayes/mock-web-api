aa52fe416591c4693d23a28973a3a900433ae08d
Austin A <austin.ascura@gmail.com>
2018-06-07 11:00:55 -0400

    adding test dependencies

---
+++ MockWebApi.UnitTests/UserRepositoryTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using MongoDA;
+using MongoDB.Driver;
+
+namespace MockWebApi.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
