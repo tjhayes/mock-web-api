8fea556d4f0c217676918ac13db41fb808eec9ce
Timothy Hayes <tjhayes011@gmail.com>
2018-06-06 21:53:23 -0400

    add users controller

---
+++ MockWebApi/Controllers/MongoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDA;
+using MongoDB.Driver;
+
+namespace MockWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/users")]
+    public class MongoController : Controller
+    {
+        private readonly UserRepository _repo;
+
+        public MongoController()
+        {
+            _repo = new UserRepository("mongodb://localhost", "usersdb", "users");
+        }
+
+        //public MongoController(string mongoDbConnectionString, string databaseName, string usersTableName)
+        //{
+        //    _repo = new UserRepository(mongoDbConnectionString, databaseName, usersTableName);
+        //}
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return Json(_repo.Get());
+        }
+    }
+}
