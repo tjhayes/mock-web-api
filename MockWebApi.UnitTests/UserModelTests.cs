b75f0ddcb78ec65131932b7090ce31f328313bf7
Timothy Hayes <tjhayes011@gmail.com>
2018-06-09 11:44:51 -0400

    Add model validation logic

---
+++ MockWebApi.UnitTests/UserModelTests.cs
@@ -0,0 +1,24 @@
+﻿using MongoDA;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MockWebApi.UnitTests
+{
+    public class UserModelTests
+    {
+        private readonly User _user;
+
+        public UserModelTests()
+        {
+            _user = new User();
+        }
+
+        [Fact]
+        public void DefaultUserShouldBeInvalid()
+        {
+            Assert.False(_user.Validate());
+        }
+    }
+}
--- MongoDA/User.cs
+++ MongoDA/User.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 using System.Text;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Globalization;
 
 namespace MongoDA
 {
@@ -26,6 +27,54 @@ namespace MongoDA
         public string Gender { get; set; }
         [DataMember]
         public string Type { get; set; }
+
+        /// <summary>
+        /// Returns true if the User model is valid, and false otherwise.
+        /// </summary>
+        /// <remarks>
+        /// All fields are required except Address, thus if any field besides
+        /// Address is null or the Guid is the default value, the model is invalid.
+        /// If Address is not null, the Address must be valid for the User to 
+        /// be valid (via Address's validate method)
+        /// Name must be valid for the User to be valid also (via Name's validate method).
+        /// </remarks>
+        /// <returns>True if user model is valid and false if invalid.</returns>
+        public Boolean Validate()
+        {
+            if(UserId == Guid.Empty) { return false; }
+            if(Location == null || Location == "") { return false; }
+            if(Address != null && Address.Validate() == false) { return false; }
+            if(Email == null || ValidateEmail() == false) { return false; }
+            if(Name == null || Name.Validate() == false) { return false; }
+            if(Gender == null || ValidateGender() == false) { return false; }
+            if(Type == null || Type == "") { return false; }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Validate email with regex.
+        /// </summary>
+        /// <returns>True if the email is valid and false otherwise.</returns>
+        public Boolean ValidateEmail()
+        {
+            // TODO: check regex for email
+            return true;
+        }
+
+        /// <summary>
+        /// Check that the Gender field is a recognizable way to represent
+        /// Male or Female.
+        /// </summary>
+        /// <remarks>
+        /// Valid gender strings are "M", "Male", "F", "Female". (all case-insensitive)
+        /// </remarks>
+        /// <returns>True if the Gender is valid and false otherwise.</returns>
+        public Boolean ValidateGender()
+        {
+            // TODO: check regex for gender
+            return true;
+        }
     }
 
     [DataContract]
@@ -40,6 +89,24 @@ namespace MongoDA
         public string Middle { get; set; }
         [DataMember]
         public string Last { get; set; }
+
+        /// <summary>
+        /// Check that the Name object represents a valid name.
+        /// </summary>
+        /// <remarks>
+        /// First and Last names are required, as well as Name Id. 
+        /// Middle name is optional.
+        /// No name fields can be an empty string.
+        /// </remarks>
+        /// <returns>True if the name is valid and false otherwise.</returns>
+        public Boolean Validate()
+        {
+            if(NameId == Guid.Empty) { return false; }
+            if(first == null || first == "") { return false; }
+            if(Middle == "") { return false; }
+            if(Last == null || Last == "") { return false; }
+            return true;
+        }
     }
 
     [DataContract]
@@ -60,5 +127,69 @@ namespace MongoDA
         public string PostalCode { get; set; }
         [DataMember]
         public string Country { get; set; }
+
+        /// <summary>
+        /// Check whether the address is valid.
+        /// </summary>
+        /// <remarks>
+        /// AddressId, Address1, City, State, PostalCode and Country are all required.
+        /// Address2 is not required but cannot be an empty string.
+        /// If the country is the United States, postal codes must follow the
+        /// 5-digit convention (with or without the 4-digit extension code).
+        /// Country must follow the ISO Alpha-2 country code format, 
+        /// e.g. US for United States and GB for United Kingdom.
+        /// </remarks>
+        /// <returns>True if the address is valid and false otherwise</returns>
+        public Boolean Validate()
+        {
+            if(AddressId == Guid.Empty) { return false; }
+            if(Address1 == null || Address1 == "") { return false; }
+            if(Address2 == "") { return false; }
+            if(City == null || City == "") { return false; }
+            if(State == null || State == "") { return false; }
+            if(Country == null || ValidateCountry() == false) { return false; }
+            if(Country.ToUpper() == "US" && ValidateAmericanState() == false) { return false; }
+            if(Country.ToUpper() == "US" && ValidateAmericanPostalCode() == false) { return false; }
+            return true;
+        }
+
+        /// <summary>
+        /// Check whether Country is a valid ISO Alpha-2 country code.
+        /// </summary>
+        /// <remarks>
+        /// Try to construct a RegionInfo object with the Country string.
+        /// If RegionInfo constructor throws ArgumentException,
+        /// then Country is not a valid ISO Alpha-2 country code.
+        /// Thus Country is invalid. If no exception is thrown, Country is valid.
+        /// </remarks>
+        /// <returns>True if Country is valid, and false otherwise</returns>
+        public Boolean ValidateCountry()
+        {
+            bool validCountry = true;
+            try { RegionInfo region = new RegionInfo(Country); }
+            catch(ArgumentException e) { validCountry = false; }
+            return validCountry; 
+        }
+
+        /// <summary>
+        /// Check whether the string is one of the valid 2-digit American
+        /// State codes. If so, it is valid, otherwise invalid.
+        /// </summary>
+        /// <returns>True if the state code is valid, and false otherwise.</returns>
+        public Boolean ValidateAmericanState()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Check whether the postal code is in the 5-digit ZIP or the ZIP+4
+        /// postal code format for American postal codes. 
+        /// If it is, then it is valid. Otherwise it is invalid.
+        /// </summary>
+        /// <returns>True if postal code is in a valid format and false otherwise.</returns>
+        public Boolean ValidateAmericanPostalCode()
+        {
+            return true;
+        }
     }
 }
