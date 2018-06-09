486ecb5892c992b54e04eb73353194858a8c3da8
Timothy Hayes <tjhayes011@gmail.com>
2018-06-09 09:48:43 -0400

    add Repository interface

---
+++ MongoDA/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MongoDA
+{
+    /// <summary>
+    /// Repository interface for User model with CRUD functionality.
+    /// </summary>
+    public interface IUserRepository
+    {
+        void Insert(User user);
+        IQueryable<User> Get();
+        User GetById(Guid id);
+        void Update(User user);
+        void Delete(Guid id);
+        void DeleteAll();
+    }
+}
